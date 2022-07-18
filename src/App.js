27157ed9b7c1ab13d8db469879fe98046f482260
Shuhao <loveyu5566@outlook.com>
2022-07-18 09:19:52 +0800

    debug loading-loop.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer/Footer';
 import BackgroundWaveAnimation from './components/BackgroundWaveAnimation';
 
-import Index from './pages/member/Index';
+// import Index from './pages/member/Index';
 import Cart from './pages/Cart';
-import Products from './pages/Products-Yu';
+// import Products from './pages/Products-Yu';
 // import Membercenter from './pages/member/Membercenter';
 // import Login from './pages/member/Login';
 // import Eye from './components/Member/Eye'
@@ -15,7 +15,7 @@ function App() {
     <>
       <BackgroundWaveAnimation />
       <Navbar />
-      <Products />
+      <Cart />
       <Footer />
     </>
   );
