87717ee0f73e4968c1f17d03909a270074d7e62e
Shuhao <loveyu5566@outlook.com>
2022-07-19 15:18:13 +0800

    creat page router in app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
-import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import BackgroundWaveAnimation from './components/BackgroundWaveAnimation';
 import './App.css';
 
-// import Index from './pages/member/Index';
-// import Cart from './pages/Cart';
+import Cart from './pages/Cart';
 import ProductsYu from './pages/Products-Yu/Products-Yu';
 import Membercenter from './pages/member/Membercenter';
 import Login from './pages/member/Login';
-// import Eye from './components/Member/Eye'
-// import Member_register from './pages/member/Member_register'
+import Member_register from './pages/member/Member_register';
 
 // react router 錄影檔 7/19 14:33
 function App() {
+  // 設定會員登入狀態 錄影檔 7/19 14:55
+  const [auth, setAuth] = useState(false);
+
   return (
     <BrowserRouter>
       <Navbar />
+
+      {/* 路由表 */}
       <Routes>
+        {/* LoveYu的頁面 */}
         <Route path="ProductsYu" element={<ProductsYu />} />
+        <Route path="Cart" element={<Cart />} />
+
+        {/* Yu的頁面 */}
+        <Route path="Login" element={<Login />} />
+        <Route path="Membercenter" element={<Membercenter />} />
+        <Route path="Member_register" element={<Member_register />} />
       </Routes>
+
       <Footer />
       <BackgroundWaveAnimation />
     </BrowserRouter>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Navbar.css';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function Navbar() {
   return (
