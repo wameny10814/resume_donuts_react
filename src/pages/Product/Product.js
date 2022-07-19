eafea3f691976c7f03d0d97d4826ec82502abf9f
Mars Chng <wayne.chuang.27@gmail.com>
2022-07-19 22:23:55 +0800

    介面初稿

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,5 +1,20 @@
+import './Product.css';
+
 function Product() {
-    
+  return (
+    <>
+      <div className="Mars-container">
+        <div className="Mars-img-wrap">
+          <img src="./images/mv_donut.jpg" className="Mars-visual" alt=""></img>
+        </div>
+        <div className="Mars-breadcrumb"></div>
+        <div className=""></div>
+        <div className=""></div>
+        <div className=""></div>
+        <div className="Mars-product-cards col-4 d-flex"></div>
+      </div>
+    </>
+  );
 }
 
-export default Product
+export default Product;
