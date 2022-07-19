712d56a77ede0d56a5a98baf3c8a23a46c4bb0b9
Shuhao <loveyu5566@outlook.com>
2022-07-19 14:58:14 +0800

    Cretae ReactRouter in app.js.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer/Footer';
 import BackgroundWaveAnimation from './components/BackgroundWaveAnimation';
-import './App.css'
+import './App.css';
 
 // import Index from './pages/member/Index';
 // import Cart from './pages/Cart';
-import Products from './pages/Products-Yu/Products-Yu';
+import ProductsYu from './pages/Products-Yu/Products-Yu';
 import Membercenter from './pages/member/Membercenter';
 import Login from './pages/member/Login';
 // import Eye from './components/Member/Eye'
 // import Member_register from './pages/member/Member_register'
 
-
+// react router 錄影檔 7/19 14:33
 function App() {
   return (
-    <>
-      <BackgroundWaveAnimation />
+    <BrowserRouter>
       <Navbar />
-      <Membercenter />
+      <Routes>
+        <Route path="ProductsYu" element={<ProductsYu />} />
+      </Routes>
       <Footer />
-    </>
+      <BackgroundWaveAnimation />
+    </BrowserRouter>
   );
 }
 
