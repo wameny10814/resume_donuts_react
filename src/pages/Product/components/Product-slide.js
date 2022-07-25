521216f12ba32c750fab93c523072293291854e5
Mars Chng <wayne.chuang.27@gmail.com>
2022-07-25 14:59:14 +0800

    商品頁初步架構

---
+++ src/pages/Product/components/Product-slide.js
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import '../Product.css'
+
+function ProductSlide() {
+    return (
+        // <ProductSlideCard />
+        <div className="Mars-prod-wrap">
+            <div className="Mars-prod-name">宇治抹茶那堤</div>
+            <div className="Mars-prod-price">NT$ 35</div>
+            <button>加入購物車</button>
+        </div>
+    )
+}
+
+export default ProductSlide
