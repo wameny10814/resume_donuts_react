51fe58061d7023d0d895c5cc9cb9b50da38b8f19
binghan60 <binghan60@gmail.com>
2022-07-26 22:43:24 +0800

    桌機分享牆完成

--- src/pages/About/Share.js
+++ src/pages/About/Share.js
@@ -2,47 +2,66 @@ import H2 from './H2';
 function Share() {
   return (
     <>
-      <section className="Share">
-        <H2 title="會員分享" Entitle="SOCIAL" />
-        <div className="container col-12 d-flex">
-          <div className="col-6 d-flex flex-wrap">
-            <div className="w-50">
-              <img
-                className="bingShare w-100"
-                src="./images/Share-1.svg"
-                alt=""
-              />
-            </div>
-            <div className="w-50">
-              <img
-                className="bingShare w-100"
-                src="./images/Share-2.svg"
-                alt=""
-              />
-            </div>
-            <div className="col-4 d-flex flex-wrap">
-              <div className="col-12">
-                <img className="bingShare " src="./images/Share-3.svg" alt="" />
+      <H2 title="會員分享" Entitle="SOCIAL" />
+
+      <section className="container">
+        <div className="d-flex justify-content-center Share">
+          <div className="col-md-5">
+            <div className="d-flex">
+              <div className="w-50">
+                <img
+                  className="w-100 h-100"
+                  src="./images/Share-1.svg"
+                  alt=""
+                />
               </div>
-              <div className="col-12">
-                <img className="bingShare" src="./images/Share-4.svg" alt="" />
+              <div className="w-50">
+                <img
+                  className="w-100 h-100"
+                  src="./images/Share-2.svg"
+                  alt=""
+                />
               </div>
             </div>
-            <div className="col-8">
-              <img className="bingShare" src="./images/Share-5.svg" alt="" />
+            <div className="d-flex">
+              <div className="ShareW-40">
+                <div className="h-50">
+                  <img
+                    className="w-100 h-100"
+                    src="./images/Share-3.svg"
+                    alt=""
+                  />
+                </div>
+                <div className="h-50">
+                  <img
+                    className="w-100 h-100"
+                    src="./images/Share-4.svg"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="ShareW-60">
+                <img
+                  className="w-100 h-100"
+                  src="./images/Share-5.svg"
+                  alt=""
+                />
+              </div>
             </div>
           </div>
-          <div className="col-6 d-flex flex-wrap">
-            <div className="col-5 ">
-              <img className="bingShare" src="./images/Share-6.svg" alt="" />
+          <div className="col-md-2">
+            <img className="h-100" src="./images/Share-6.svg" alt="" />
+          </div>
+          <div className="col-md-4">
+            <div className="h-50">
+              <img className="w-100" src="./images/Share-7.svg" alt="" />
             </div>
-            <div className="col-7">
-              <div className="col-6">
-                <img className="bingShare" src="./images/Share-7.svg" alt="" />
-              </div>
-              <div className="col-6">
-                <img className="bingShare" src="./images/Share-8.svg" alt="" />
-              </div>
+            <div className="h-50">
+              <img
+                className="w-100 h-100 overflow-hidden"
+                src="./images/Share-8.svg"
+                alt=""
+              />
             </div>
           </div>
         </div>
