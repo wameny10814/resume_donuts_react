7d27f38f144665e098cf672691d4f84c28164d98
Mars Chng <wayne.chuang.27@gmail.com>
2022-07-28 15:36:26 +0800

    商品頁 button

--- src/pages/Product/components/Product-slide.js
+++ src/pages/Product/components/Product-slide.js
@@ -10,7 +10,7 @@ function ProductSlide() {
       </div>
       <p className="Mars-prod-name">宇治抹茶那堤</p>
       <p className="Mars-prod-price">NT$ 35</p>
-      <button className="Mars-cart-btn">加入購物車</button>
+      <button className="Mars-cart-btn"><i class="fa-solid fa-circle-plus"></i> 加入購物車</button>
     </div>
   );
 }
