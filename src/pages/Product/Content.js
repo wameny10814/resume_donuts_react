05e2af0b28f2dc88a814c317a575554d8e4510e8
Mars Chng <wayne.chuang.27@gmail.com>
2022-07-29 14:15:29 +0800

    商品內容頁切版

--- src/pages/Product/Content.js
+++ src/pages/Product/Content.js
@@ -7,7 +7,8 @@ function Content() {
         <div className="Mars-breadcrumb"></div>
         <div className="d-flex">
           <div className="Mars-cnt-imgwr">
-            <img src="./images/uji-matcha.jpg" alt="" />
+            {/* eslint-disable-next-line prettier/prettier */}
+            <img src="./images/uji-matcha.jpg" alt="" className="Mars-cnt-img" />
           </div>
           <div className="Mars-cnt-info">
             <p className="Mars-prod-name">宇治抹茶那提</p>
