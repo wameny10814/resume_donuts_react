55b8d87a777898d8fb2f921526fddde64e1967f4
Mars Chng <wayne.chuang.27@gmail.com>
2022-08-01 17:20:08 +0800

    整理程式碼，任務註解

--- src/pages/Product/components/Img-onclick.js
+++ src/pages/Product/components/Img-onclick.js
@@ -1,7 +1,8 @@
 function ImgOnClick(params) {
   const ImgWrap = document.querySelector('.Mars-cnt-imgwr');
   ImgWrap.style = 'box-shadow: 0 5px 25px 0 rgb(0 0 0 / 10%);';
-  // document.getElementById("Mars-img-wrap").innerHTML =
 }
 
 export default ImgOnClick;
+
+// Plan A: eventListener hover out 後取消陰影
