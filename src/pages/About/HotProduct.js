68206a88d2ba6b1b1bb05cfd45ab1d0c2deb2472
binghan <binghan60@gmail.com>
2022-08-03 15:27:10 +0800

    輪播牆切版

--- src/pages/About/HotProduct.js
+++ src/pages/About/HotProduct.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import H2 from './H2';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
 
-function HotProduct() {
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import H2 from '../../components/H2';
+
+// import required modules
+import { Pagination, Navigation } from 'swiper';
+
+export default function App() {
   return (
     <>
       <H2 title="人氣商品" Entitle="HOT" />
+
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        slidesPerGroup={1}
+        loop={true}
+        loopFillGroupWithBlank={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="mx-auto col-md-10">
+            <img className="w-100 h-100" src="./images/Hot-1.svg" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="mx-auto col-md-10">
+            <img className="w-100 h-100" src="./images/Hot-2.svg" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="mx-auto col-md-10">
+            <img className="w-100 h-100" src="./images/Hot-3.svg" alt="" />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="mx-auto col-md-10">
+            <img className="w-100 h-100" src="./images/Hot-2.svg" alt="" />
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 }
-
-export default HotProduct;
