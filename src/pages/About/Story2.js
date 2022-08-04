e7fbe8f4fc6a441de13dccc4861f42eaa79d4320
binghan <binghan60@gmail.com>
2022-08-04 10:24:25 +0800

    測試筆電Story2排版

--- src/pages/About/Story2.js
+++ src/pages/About/Story2.js
@@ -11,21 +11,11 @@ function Story2() {
             <p className="col-12 col-md-9 bingText-16">
               處在講究飲食健康、營養均衡的時代，Pochi屋堅持使用天然有機食材，為顧客提供品項眾多的選擇，這一切都是為了傳達健康、友善環境的理念。
             </p>
-            <img
-              className="bingflag1 position-relative mx-3"
-              src="./images/flag1.svg"
-              alt=""
-            />
-            <img
-              className="bingflag2 position-relative mx-3"
-              src="./images/flag2.svg"
-              alt=""
-            />
-            <img
-              className="bingflag3 position-relative mx-3"
-              src="./images/flag3.svg"
-              alt=""
-            />
+            <div className="col-12">
+              <img className="mt-5 pt-5" src="./images/flag1.svg" alt="" />
+              <img className="mt-5" src="./images/flag2.svg" alt="" />
+              <img className="" src="./images/flag3.svg" alt="" />
+            </div>
           </div>
         </div>
       </section>
