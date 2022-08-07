1557e0a45fedd3a61a13dfac50f9be04e0c71c40
binghan60 <binghan60@gmail.com>
2022-08-07 23:29:39 +0800

    客製化構思中

--- src/pages/About/Customized.js
+++ src/pages/About/Customized.js
@@ -1,14 +1,33 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import H2 from '../../components/H2';
 
 function Customized() {
-  const [wheel, setWheel] = useState('origin');
+  const cuscanvas = useRef(null);
+  //挑選本體
+  const [tasteImg, setTasteImg] = useState('');
 
-  const selectWheel = (e) => {
-    const newWheel = e.target.value;
-    setWheel(newWheel);
-    console.log(newWheel);
-  };
+  const [tasteName, setTasteName] = useState(['origin']);
+  const tasteNameOptions = ['origin', 'strawberry', 'matcha', 'chocolate'];
+  //挑選配料
+  const [ingredientsName, setIngredientsName] = useState(['']);
+  const ingredientsNameOptions = ['sugar', 'cotton', 'chocolate2'];
+
+  useEffect(() => {
+    const backimg = new Image();
+    backimg.src = `/imgs/Customized/${tasteName}.png`;
+    // backimg.src = '/imgs/Customized/style_01.png';
+
+    backimg.onload = () => {
+      setTasteImg(backimg);
+    };
+  }, [tasteName]);
+
+  useEffect(() => {
+    if (tasteImg && cuscanvas) {
+      const ctx = cuscanvas.current.getContext('2d');
+      ctx.drawImage(tasteImg, 0, 0, 0, 0);
+    }
+  }, [tasteImg, cuscanvas]);
 
   return (
     <>
@@ -16,83 +35,55 @@ function Customized() {
         <H2 title={'製作獨特的甜甜圈'} Entitle={'Customized'}></H2>
         <div className="d-flex">
           <div className="col-12 col-md-4 bingControl h-100">
-            <h6>請選擇基本口味</h6>
-            <input
-              type="radio"
-              value="origin"
-              name="cus_wheel"
-              onChange={selectWheel}
-            />
-            <label>原味</label>
-            <img className="w-25" src="./images/Customized/origin.jpg" alt="" />
-            <input
-              type="radio"
-              value="strawberry"
-              name="cus_wheel"
-              onChange={selectWheel}
-            />
-            <label>草莓</label>
-            <img
-              className="w-25"
-              src="./images/Customized/strawberry.jpg"
-              alt=""
-            />
-            <input
-              type="radio"
-              value="matcha"
-              name="cus_wheel"
-              onChange={selectWheel}
-            />
-            <label>抹茶</label>
-            <img className="w-25" src="./images/Customized/matcha.jpg" alt="" />
-            <input
-              type="radio"
-              value="chocolate"
-              name="cus_wheel"
-              onChange={selectWheel}
-            />
-            <label>巧克力</label>
-            <img
-              className="w-25"
-              src="./images/Customized/chocolate.jpg"
-              alt=""
-            />
-            <h6>請選擇配料</h6>
-            <input
-              type="radio"
-              value="sugar"
-              name="cus_wheel"
-              onChange={selectWheel}
-            />
-            <label>糖粉</label>
-            <img className="w-25" src="./images/Customized/sugar.jpg" alt="" />
-            <input
-              type="radio"
-              value="chocolate2"
-              name="cus_wheel"
-              onChange={selectWheel}
-            />
-            <label>巧克力</label>
-            <img
-              className="w-25"
-              src="./images/Customized/chocolate2.jpg"
-              alt=""
-            />
-            <input
-              type="radio"
-              value="cotton"
-              name="cus_wheel"
-              onChange={selectWheel}
-            />
-            <label>棉花糖</label>
-            <img className="w-25" src="./images/Customized/cotton.jpg" alt="" />
+            <h6>挑選甜甜圈口味(單選)</h6>
+            {tasteNameOptions.map((v, i) => {
+              return (
+                <div key={i}>
+                  <input
+                    type="radio"
+                    checked={tasteName === v}
+                    value={v}
+                    onChange={(e) => {
+                      setTasteName(e.target.value);
+                    }}
+                  />
+                  <label>{v}</label>
+                </div>
+              );
+            })}
+            <h6>挑選甜甜圈口味(多選)</h6>
+            {ingredientsNameOptions.map((v, i) => {
+              return (
+                <div key={i}>
+                  <input
+                    type="checkbox"
+                    checked={ingredientsName.includes(v)}
+                    value={v}
+                    onChange={(e) => {
+                      if (ingredientsName.includes(e.target.value)) {
+                        const newIngredients = ingredientsName.filter(
+                          (v, i) => {
+                            return v !== e.target.value;
+                          }
+                        );
+                        setIngredientsName(newIngredients);
+                      } else {
+                        const newIngredients = [
+                          ...ingredientsName,
+                          e.target.value,
+                        ];
+                        setIngredientsName(newIngredients);
+                      }
+                    }}
+                  />
+                  <label>{v}</label>
+                </div>
+              );
+            })}
           </div>
+
           <div className="col-12 col-md-8">
-            <img
-              className="w-100"
-              src={`./images/Customized/${wheel}.jpg`}
-              alt=""
-            />
+            <canvas ref={cuscanvas} width={100} height={500} />
           </div>
         </div>
       </div>
