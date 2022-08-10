bdddd7f0c9529193e6fd99185290cfc50d90ae17
Mars Chng <wayne.chuang.27@gmail.com>
2022-08-10 13:01:30 +0800

    純參考內容/ just some references

--- src/pages/Product/components/GetInfosFromProdPage.js
+++ src/pages/Product/components/GetInfosFromProdPage.js
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 
 function GetInfosFromProdPage() {
-    const [infos, setInfos] = useState['']
-    const infosOnChange = setInfos(infos)
-
-    
+  const [infos, setInfos] = useState[''];
+  const infosOnChange = setInfos(infos);
 }
 
 // 一樣用 v.sid 傳送到商品內容頁 (Content.js)
 // e.target.value
+
+// 參考：小汪的localStorage
+// useEffect(() => {
+//     getData();
+//     localStorage.setItem("Room", JSON.stringify(bookingList));
+// }, [bookingList]);
