918f3319f493607ced58d1dc5f2e2b6ffd6fdbba
Mars Chng <wayne.chuang.27@gmail.com>
2022-08-11 11:14:20 +0800

    繼續參考學長姐code

--- src/pages/Product/components/GetInfosFromProdPage.js
+++ src/pages/Product/components/GetInfosFromProdPage.js
@@ -15,6 +15,48 @@ function GetInfosFromProdPage() {
   //   setProductRelated(data.related)
   // }
 
+  // async function getProductIdServer() {
+  //   const url = `http://localhost:6005/product/product-list/${id}`
+
+  //   // 注意header資料格式要設定，伺服器才知道是json格式
+  //   const request = new Request(url, {
+  //     method: 'GET',
+  //     headers: new Headers({
+  //       Accept: 'application/json',
+  //       'Content-Type': 'appliaction/json',
+  //     }),
+  //   })
+
+  //   const response = await fetch(request)
+  //   const data = await response.json()
+  //   const imgData = data.proMutImg
+  //   const imgArr = JSON.parse(imgData)
+  //   // console.log(data)
+  //   // console.log(imgData)
+  //   // console.log(imgArr)
+
+  //   setProMutImgTry(imgArr)
+
+  //   // 設定資料
+  //   console.log('data', data)
+  //   setProduct(data)
+  //   setProId(data.proId)
+  //   setProName(data.proName)
+  //   setProPrice(data.proPrice)
+  //   setProDes(data.proDes)
+  //   setProClass(data.proClass)
+  //   setProNum(id)
+  //   setSqlProductId(data.proId)
+
+  //   // setStarValue(data.starValue)
+  //   // setUserId(data.userId)
+  //   // setComments(data.comments)
+  //   console.log('proNum', proNum)
+  // }
+  // useEffect(() => {
+  //   getProductIdServer()
+  // }, [])
+
 //   return (
 //     <div className="Mars-" onChange={infosOnChange}></div>
 //   )
