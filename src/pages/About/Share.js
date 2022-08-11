e039083ff7fd0808da77cde9f11505e2d129978a
binghan <binghan60@gmail.com>
2022-08-11 14:20:41 +0800

    資料顯示正常

--- src/pages/About/Share.js
+++ src/pages/About/Share.js
@@ -2,15 +2,8 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import H2 from '../../components/H2';
 function Share() {
-  const [share1Data, setShare1Data] = useState([]);
-  const [share2Data, setShare2Data] = useState([]);
-  const [share3Data, setShare3Data] = useState([]);
-  const [share4Data, setShare4Data] = useState([]);
-  const [share5Data, setShare5Data] = useState([]);
-  const [share6Data, setShare6Data] = useState([]);
-  const [share7Data, setShare7Data] = useState([]);
-  const [share8Data, setShare8Data] = useState([]);
-
+  const [shareData, setShareData] = useState([]);
+  
   //資料結構
   //   good_at: "2022-08-11T03:42:53.000Z"
   // goodimg: "732a871b-6268-47d5-a211-abb05369f218.jpg"
@@ -24,14 +17,7 @@ function Share() {
       `http://localhost:3600/willownews/goodwritingdata`
     );
     const resdata = response.data;
-    setShare1Data(resdata[0]);
-    setShare2Data(resdata[1]);
-    setShare3Data(resdata[2]);
-    setShare4Data(resdata[3]);
-    setShare5Data(resdata[4]);
-    setShare6Data(resdata[5]);
-    setShare7Data(resdata[6]);
-    setShare8Data(resdata[7]);
+    setShareData(resdata);
   };
 
   useEffect(() => {
@@ -40,78 +26,79 @@ function Share() {
 
   return (
     <>
-      {console.log('qweqwe', share1Data.goodimg)}
-      <section className="container">
-        <H2 title="好文分享" Entitle="SHARE" />
-        <div className="d-flex justify-content-center share">
-          <div className="col-md-6">
-            <div className="d-flex h-50 w-100">
-              <div className="w-50">
-                <img
-                  className="w-100 h-100"
-                  src={`http://localhost:3600/willowimgs/${share1Data.goodimg}`}
-                  alt=""
-                />
-              </div>
-              <div className="w-50">
-                <img
-                  className="w-100 h-100"
-                  src={`http://localhost:3600/willowimgs/${share2Data.goodimg}`}
-                  alt=""
-                />
-              </div>
-            </div>
-            <div className="d-flex h-50 w-100">
-              <div className="shareW-40">
-                <div className="h-50">
+      {shareData.length > 0 && (
+        <section className="container">
+          <H2 title="好文分享" Entitle="SHARE" />
+          <div className="d-flex justify-content-center share">
+            <div className="col-md-6">
+              <div className="d-flex h-50 w-100">
+                <div className="w-50">
                   <img
                     className="w-100 h-100"
-                    src={`http://localhost:3600/willowimgs/${share3Data.goodimg}`}
+                    src={`http://localhost:3600/willowimgs/${shareData[0].goodimg}`}
                     alt=""
                   />
                 </div>
-                <div className="h-50">
+                <div className="w-50">
                   <img
                     className="w-100 h-100"
-                    src={`http://localhost:3600/willowimgs/${share4Data.goodimg}`}
+                    src={`http://localhost:3600/willowimgs/${shareData[1].goodimg}`}
                     alt=""
                   />
                 </div>
               </div>
-              <div className="shareW-60">
+              <div className="d-flex h-50 w-100">
+                <div className="shareW-40">
+                  <div className="h-50">
+                    <img
+                      className="w-100 h-100"
+                      src={`http://localhost:3600/willowimgs/${shareData[2].goodimg}`}
+                      alt=""
+                    />
+                  </div>
+                  <div className="h-50">
+                    <img
+                      className="w-100 h-100"
+                      src={`http://localhost:3600/willowimgs/${shareData[3].goodimg}`}
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="shareW-60">
+                  <img
+                    className="w-100 h-100"
+                    src={`http://localhost:3600/willowimgs/${shareData[4].goodimg}`}
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="col-md-2">
+              <img
+                className="w-100 h-100"
+                src={`http://localhost:3600/willowimgs/${shareData[5].goodimg}`}
+                alt=""
+              />{' '}
+            </div>
+            <div className="col-md-4">
+              <div className="h-50">
                 <img
                   className="w-100 h-100"
-                  src={`http://localhost:3600/willowimgs/${share5Data.goodimg}`}
+                  src={`http://localhost:3600/willowimgs/${shareData[6].goodimg}`}
                   alt=""
-                />
+                />{' '}
+              </div>
+              <div className="h-50">
+                <img
+                  className="w-100 h-100"
+                  src={`http://localhost:3600/willowimgs/${shareData[7].goodimg}`}
+                  alt=""
+                />{' '}
               </div>
             </div>
           </div>
-          <div className="col-md-2">
-            <img
-              className="w-100 h-100"
-              src={`http://localhost:3600/willowimgs/${share6Data.goodimg}`}
-              alt=""
-            />{' '}
-          </div>
-          <div className="col-md-4">
-            <div className="h-50">
-              <img
-                className="w-100 h-100"
-                src={`http://localhost:3600/willowimgs/${share7Data.goodimg}`}
-                alt=""
-              />{' '}
-            </div>
-            <div className="h-50">
-              <img
-                className="w-100 h-100"
-                src={`http://localhost:3600/willowimgs/${share8Data.goodimg}`}
-                alt=""
-              />{' '}
-            </div>
-          </div>
-        </div>
-      </section>
+        </section>
+      )}
     </>
   );
 }
