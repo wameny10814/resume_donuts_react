b72a3b6ce1393d1a2827aa1bfd9a0e06d5ba04e2
Mars Chng <wayne.chuang.27@gmail.com>
2022-08-12 17:13:48 +0800

    繼續建構

--- src/pages/Product/Content.js
+++ src/pages/Product/Content.js
@@ -45,6 +45,7 @@ function Content() {
           >
             {/* eslint-disable-next-line prettier/prettier */}
             <img src="../images/uji-matcha.jpg" alt="" className="Mars-cnt-img" />
+            {/* img src={`../{product.product_image}`} */}
           </div>
           {/* <div className="Mars-cnt-imgwr">
             <img src="./images/uji-matcha.jpg" alt="" className="Mars-cnt-img"/>
