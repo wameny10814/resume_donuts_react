d2bf6a630e8b359c1b151ca95e69714136f0c961
Shuhao <loveyu5566@outlook.com>
2022-08-12 19:01:30 +0800

    新增信用卡及資料判斷

--- src/pages/CartOrderInfo/CartOrderInfo.js
+++ src/pages/CartOrderInfo/CartOrderInfo.js
@@ -1,19 +1,30 @@
 import React, { useState, useContext, createContext } from 'react';
 import H2 from '../../components/H2';
 
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import MenuItem from '@mui/material/MenuItem';
-
 import PersonalInfo from './components/PersonalInfo';
 import CurrentOrder from './components/CurrentOrder';
+import CreditCard from './components/CreditCard';
+import ProjectButton from '../../components/ProjectButton/ProjectButton';
 
 function CartOrderInfo() {
+  // 信用卡資料
+  const [creditCardData, setCreditCardData] = useState({
+    creditCardNum: '',
+    creditCardDate: '',
+    creditCardName: '',
+    creditSecurityCode: '',
+  });
+
   return (
     <div className="container">
       <H2 title="訂單資訊" Entitle="ORDER INFO" />
       <CurrentOrder />
       <PersonalInfo />
+      <CreditCard
+        creditCardData={creditCardData}
+        setCreditCardData={setCreditCardData}
+      />
+      <ProjectButton className="w-25" text="下一步" />
     </div>
   );
 }
