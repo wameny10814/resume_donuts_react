75314f463d4d68750486e1f74e10808350cf6c30
binghan60 <binghan60@gmail.com>
2022-08-19 00:14:58 +0800

    客製化排版

--- src/pages/About/BingCustomized.js
+++ src/pages/About/BingCustomized.js
@@ -92,6 +92,13 @@ function BingCustomized() {
 
   //寫入資料庫
   const logsee = async (e) => {
+    setDonut('origin');
+    setDonutPrice(15);
+    setLayer('');
+    setLayerPrice(0);
+    setDecoration('');
+    setDecorationPrice(0);
+    //data等於主狀態寫入資料表
     const data = main;
 
     const response = await axios.post(
@@ -110,115 +117,131 @@ function BingCustomized() {
     <>
       <div className="container h-100">
         <H2 title={'製作獨特的甜甜圈'} Entitle={'Customized'}></H2>
-        <div className="d-flex flex-wrap">
-          <div className="col-12 col-md-4 h-100">
-            <h6>挑選甜甜圈口味</h6>
-            <div className="d-flex flex-wrap">
+        <div className="d-flex flex-wrap justify-content-center">
+          <div className="col-12 col-md-5 h-100">
+            <h6 className="bingText-16">挑選甜甜圈口味</h6>
+            <div className="d-flex">
               {donutOptions.map((v, i) => {
                 return (
-                  <div key={i}>
-                    <input
-                      id="donut"
-                      type="radio"
-                      checked={donut === v}
-                      value={v}
-                      onChange={(e) => {
-                        setDonut(e.target.value);
-                        setDonutPrice(donutPriceOptions[i]);
-                        changeFields(e);
-                      }}
-                    />
+                  <>
                     <div>
-                      <img
-                        src={`/images/Customized/basic/${v}Icon.png`}
-                        alt=""
-                      />
+                      <div className="customSelect" key={i}>
+                        <input
+                          id="donut"
+                          type="radio"
+                          checked={donut === v}
+                          value={v}
+                          onChange={(e) => {
+                            setDonut(e.target.value);
+                            setDonutPrice(donutPriceOptions[i]);
+                            changeFields(e);
+                          }}
+                        />
+                        <img
+                          src={`/images/Customized/basic/${v}Icon.png`}
+                          alt=""
+                        />
+                      </div>
                       <p className="text-center bingText-16">
                         {donutPriceOptions[i]}元
                       </p>
                     </div>
-                  </div>
+                  </>
                 );
               })}
             </div>
-            <h6>挑選糖霜</h6>
-            <div className="d-flex flex-wrap">
+            <h6 className="bingText-16">挑選糖霜</h6>
+            <div className="d-flex">
               {layerOptions.map((v, i) => {
                 return (
-                  <div key={i}>
-                    <input
-                      id="layer"
-                      type="radio"
-                      checked={layer === v}
-                      value={v}
-                      onChange={(e) => {
-                        setLayer(e.target.value);
-                        setLayerPrice(layerPriceOptions[i]);
-                        changeFields(e);
-                      }}
-                    />
+                  <>
                     <div>
-                      <img
-                        src={`/images/Customized/layer/${v}Icon.png`}
-                        alt=""
-                      />
+                      <div className="customSelect" key={i}>
+                        <input
+                          id="layer"
+                          type="radio"
+                          checked={layer === v}
+                          value={v}
+                          onChange={(e) => {
+                            setLayer(e.target.value);
+                            setLayerPrice(layerPriceOptions[i]);
+                            changeFields(e);
+                          }}
+                        />
+                        <img
+                          src={`/images/Customized/layer/${v}Icon.png`}
+                          alt=""
+                        />
+                      </div>
                       <p className="text-center bingText-16">
                         {layerPriceOptions[i]}元
                       </p>
                     </div>
-                  </div>
+                  </>
                 );
               })}
             </div>
-            <h6>挑選配料</h6>
-            <div className="d-flex flex-wrap">
+            <h6 className="bingText-16">挑選配料</h6>
+            <div className="d-flex">
               {decorationOptions.map((v, i) => {
                 return (
-                  <div key={i}>
-                    <input
-                      id="decoration"
-                      type="radio"
-                      checked={decoration === v}
-                      value={v}
-                      onChange={(e) => {
-                        setDecoration(e.target.value);
-                        setDecorationPrice(decorationPriceOptions[i]);
-                        changeFields(e);
-                      }}
-                    />
+                  <>
                     <div>
-                      <img
-                        className=""
-                        src={`/images/Customized/decoration/${v}Icon.png`}
-                        alt=""
-                      />
+                      <div className="customSelect" key={i}>
+                        <input
+                          id="decoration"
+                          type="radio"
+                          checked={decoration === v}
+                          value={v}
+                          onChange={(e) => {
+                            setDecoration(e.target.value);
+                            setDecorationPrice(decorationPriceOptions[i]);
+                            changeFields(e);
+                          }}
+                        />
+                        <img
+                          className=""
+                          src={`/images/Customized/decoration/${v}Icon.png`}
+                          alt=""
+                        />
+                      </div>
                       <p className="text-center bingText-16">
                         {decorationPriceOptions[i]}元
                       </p>
                     </div>
-                  </div>
+                  </>
                 );
               })}
             </div>
-          </div>
-          <div className="col-12 col-md-8">
-            <div className="d-flex flex-wrap">
-              <canvas ref={realRef} width={800} height={600} />
-              <div className="w-75">
-                <p className="bingH6 d-flex justify-content-between">
-                  donut：<span>+{donutPrice}元</span>
-                </p>
-                <p className="bingH6 d-flex justify-content-between">
-                  layer：<span>+{layerPrice}元</span>
-                </p>
-                <p className="bingH6 d-flex justify-content-between">
-                  decoration：<span>+{decorationPrice}元</span>
-                </p>
-                <hr />
-                <p className="text-center bingH5">目前價錢{totalPrice}元</p>
-                <button onClick={logsee}>加入購物車</button>
-              </div>
+            <div className="w-100">
+              <p className="bingText-16 d-flex justify-content-between">
+                甜甜圈口味<span>+{donutPrice}元</span>
+              </p>
+              <p className="bingText-16 d-flex justify-content-between">
+                糖霜<span>+{layerPrice}元</span>
+              </p>
+              <p className="bingText-16 d-flex justify-content-between">
+                配料<span>+{decorationPrice}元</span>
+              </p>
+              <hr />
+              <p className="d-flex justify-content-end bingH6">
+                目前價錢
+                <span className="bingH5" style={{ fontWeight: 'bold' }}>
+                  {totalPrice}
+                </span>
+                元
+              </p>
             </div>
+          </div>
+          <div className="col-12 col-md-7">
+            <div className="d-flex flex-wrap">
+              <canvas ref={realRef} width={750} height={600} />
+            </div>
+          </div>
+          <div className="text-center">
+            <button className="ProjectButton my-4" onClick={logsee}>
+              <i className="fa-solid fa-circle-plus"></i> 加入購物車
+            </button>
           </div>
         </div>
       </div>
