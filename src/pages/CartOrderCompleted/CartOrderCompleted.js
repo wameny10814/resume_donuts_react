b2d758d4dcf470929a66530236fad17e31cc087e
Shuhao <loveyu5566@outlook.com>
2022-08-19 09:39:57 +0800

    調整訂購成功頁css

--- src/pages/CartOrderCompleted/CartOrderCompleted.js
+++ src/pages/CartOrderCompleted/CartOrderCompleted.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import H2 from '../../components/H2';
 
 function CartOrderCompleted() {
@@ -6,11 +7,22 @@ function CartOrderCompleted() {
       <H2 title={'訂購成功'} Entitle={'CartOrderCompleted'} />
       <section className="cartBox">
         <div className="container">
-          <div className="card">
-
-            <p>您的商品已經訂購成功</p>
-            <p>訂單編號為</p>
-            <p>請於3日內到店取貨</p>
+          <div className="love-OrderCompletedCard">
+            <div className="loveyu-OrderCompletedFontBox">
+              <p className="love-OrderCompletedFont">您的商品已經訂購成功</p>
+              <p className="love-OrderCompletedFont">
+                訂單編號為
+                <Link className="text-decoration-none" to="/">
+                  <span>MEOW00867738</span>
+                </Link>
+              </p>
+              <p className="love-OrderCompletedFont">
+                請於<span>3日內</span>到店取貨
+              </p>
+              <Link className="text-decoration-none" to="/">
+                <button className="ProjectButton w-25">回首頁</button>
+              </Link>
+            </div>
           </div>
         </div>
       </section>
